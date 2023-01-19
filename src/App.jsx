8942627267eef8cb6c8d919bbac72d7b4b6ecf46
Saurav Pant <pantsaurav005@gmail.com>
2023-01-19 12:21:22 +0530

    Finally Tailwind Added

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <div>
+    <div class="text-xl font-medium text-black">ChitChat</div>
+    <p class="text-slate-500">You have a new message!</p>
+  </div>
+    </div>
+  )
+}
+
+export default App
